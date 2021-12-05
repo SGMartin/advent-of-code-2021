c3b2fb085f7790acc8ac6b3d8fad08c4a0442d37
SGMartin <s.g.martin94@gmail.com>
2021-12-05 22:17:40 +0100

    Add day 5 answers

--- README.md
+++ README.md
@@ -23,5 +23,7 @@
 * Day 2 (December 2, 2021): [Dive!](https://adventofcode.com/2021/day/2) - [Solution](https://gitlab.com/SGMartin/advent-of-code-2021/-/tree/main/day2).
 * Day 3 (December 3, 2021): [Binary Diagnostics](https://adventofcode.com/2021/day/3) - [Solution](https://gitlab.com/SGMartin/advent-of-code-2021/-/tree/main/day3). 
 * Day 4 (December 4, 2021): [Giant Squid](https://adventofcode.com/2021/day/4) - [Solution](https://gitlab.com/SGMartin/advent-of-code-2021/-/tree/main/day4).
+* Day 5 (December 5, 2021): [Hydrothermal venture](https://adventofcode.com/2021/day/5) - [Solution](https://gitlab.com/SGMartin/advent-of-code-2021/-/tree/main/day5).
+
 
 _Answers by SGMartin._
---
+++ day5/day5_2.py
@@ -0,0 +1,30 @@
+import re
+import numpy as np
+
+thermal_reg = r"(\d+),(\d+) -> (\d+),(\d+)"
+
+## Generates an array of 500x1 where
+## each entry is a 4n tuple
+thermal_vents = np.fromregex(file = "thermals.txt",
+                             regexp = thermal_reg,
+                             dtype = [("", int)]*4)
+
+
+## Generate our map grid. max value  -> 1000
+## Pretty sure I can plot this lol
+depths_map = np.zeros((2, 1000, 1000))
+
+for (x1, y1, x2, y2) in thermal_vents:
+    # -1 if x2 < x1
+    # +1 if x2 > x1
+    # 0 if  == aka Horizontal/vertical
+
+    x_dir = np.sign(x2 - x1)
+    y_dir = np.sign(y2 - y1)
+
+    while (x1, y1) != (x2 + x_dir, y2 + y_dir):
+        depths_map[x_dir * y_dir, x1, y1] += 1
+        x1 += x_dir
+        y1 += y_dir
+
+print(f"Any overlap points are: {(depths_map.sum(0) > 1).sum()}")
